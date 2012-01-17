e171e153b29dc141b03d4e1d243210d48d41e0d2
Shanker Dhandapany <Shanker@Vasscon.com>
2012-01-17 13:02:32 -0800

    Initial commit with bundles & Rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProbateLeads::Application.config.secret_token = 'a3d93041c2e16f4f7b5e81ab0bd8076f933f6cfd888d1c42b5fdefcb0a3bee7d2bbc35089f1c312c64426863cec57dabda38539f0159f222f844c7c0a8904c51'
